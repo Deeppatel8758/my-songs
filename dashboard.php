0939dabf32f6e6d628c88bfcfd335f5018464c76
Deeppatel8758 <69745981+Deeppatel8758@users.noreply.github.com>
2022-10-07 19:11:22 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="d_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    
+   
+</head>
+
+<body style="	background-image: url('ic.jpg');background-repeat: no-repeat;background-position: center;background-size: 1600px 1100px;">
+    <style>
+        #b1:hover{
+
+        }
+    </style>
+<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
+<div>
+  <!-- <img src="ashok.png" alt="" srcset="" height="70px" style="margin-top:45px;"> -->
+  <h1 style=" color: #bdb8d7; padding-left: 500px;font-size:38px;padding-top:0px;
+ margin-top:20px; "> Court Case And Event Status</h1>
+ <h2 style=" color: #bdb8d7; padding-left: 670px;font-size:25px;padding-top:0px;
+ margin-top:20px; "> High Court </h2>
+  <p style="color: #bdb8d7;">_______________________________________________________________________________________________________________________________________________________________________</p> 
+</div>
+
+<div class="wrapper">  
+    <div class="sidebar">
+        <h2>Main</h2>
+        <p>____________________</p>
+        <ul>
+            <li><a href="dashboard.php"><i class="fas fa-home"></i>Dashboard</a></li>
+            <li><a href="viewcase.php"><i class= "fas fa-book-open-reader"></i>View Case</a></li>
+            <li><a href="upload.php"><i class="fas fa-arrow-up-from-bracket"></i>Upload FIle</a></li>
+            <li><a href="#"><i class="fas fa-lock"></i>Logout</a></li>
+            
+        </ul> 
+   
+    </div>
+    <div>
+            
+    </div>
+    <div  id="btns" style="margin-left:500px; ">
+         <h1 style="color:black;font-size:20px;background-color:#bdb8d7; padding-left:120px;padding-top:10px;height:40px"><i style="font-size:20px; padding-right:6px;" class="fas fa-home"></i> You Are Currently At Dashboard/Home<i style="font-size:20px; padding-left:6px;" class="fas fa-home"></i></h1>
+        <button class=""  style=" height:50px; width:200px;margin-top:200px; margin-right:250px; border-radius:6px; background-color:	#c5c3d0;"><i style="font-size:20px;color:white; padding-right:6px;" class="fas fa-arrow-up-from-bracket"></i><a style="color:white;font-size:20px;
+  font-family:lucida;" href="upload.php"> Uploadfile</a></button>
+        <button   height="30px" width="100px"style="margin-top:200px; height:50px; width:200px;background-color:	#c5c3d0;border-radius:6px;"><i style="font-size:20px;color:white; padding-right:6px" class="fas fa-book-open-reader"></i><a href="#" style="  font-size:20px;
+  font-family:lucida;color:white;">ViewCase</a></button>
+    </div> 
+     
+</body>
+</html>
